d93dde90966d209565ba1d7ccd4b0e3f5c40c664
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2022-06-11 08:41:21 +0500

    Initial commit

---
+++ epythets/__init__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Поиск характерных только для последнего исследуемого текста эпитетов """
+# coding: utf-8
+
+import argparse
+import logging
+import sqlite3
+from pathlib import Path
+
+from epythets.mgrep import pick_combos
+
+
+def parse_args():
+    """ Разбор аргументов"""
+    parser = argparse.ArgumentParser()
+    parser.description = "Вытянуть последние N записей из мастодон в свой SQLite для фразочек бота"
+    parser.add_argument('--filename', type=str, help='Путь к текстовому файлу')
+    parser.add_argument('--db', required=True, type=str, help='БД для сохранения фразочек')
+    parser.add_argument('--label', type=str, help='Пометка источника для добавляемых фраз')
+    parser.add_argument('--init', action='store_true', help="Создать БД")
+    parser.add_argument('--debug', action='store_true', default=False, help="Включить отладочное логирование.")
+    parser.add_argument('--silent', action='store_true', default=False, help="Оставить только warning/error логи.")
+    args = parser.parse_args()
+    assert (args.filename is None and args.label is None) or all((args.filename, args.label))
+    return args
+
+
+def process(label, content, cur):
+    n = 0
+    for n, combo in enumerate(pick_combos(content)):
+        cur.execute(f"INSERT OR IGNORE INTO phrase (label, phrase) VALUES ('{label}', '{combo}')")
+    if n > 10:
+        logging.info("Processed line with %d phrases", n)
+
+
+def init(cur):
+    cur.execute("""create table phrase
+        (
+            phrase  text not null constraint phrase_pk primary key,
+            posted  int default 0 not null,
+            label text not null
+        );""")
+    cur.execute("create unique index phrase_phrase_uindex on phrase (phrase);")
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(level=(logging.DEBUG if args.debug else logging.WARNING if args.silent else logging.INFO),
+                        format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+    conn = sqlite3.connect(args.db)
+    cur = conn.cursor()
+    if args.init:
+        init(cur)
+    if not args.filename:
+        exit(0)
+    p = Path(args.filename)
+    assert p.is_file()
+    with p.open() as fd:
+        for count, line in enumerate(fd):
+            process(args.label, line, cur)
+            if count % 50 == 0:
+                logging.info("Processed %d lines...", count)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ epythets/test_mgrep.py
@@ -0,0 +1,22 @@
+import logging
+from unittest import TestCase
+
+from epythets.mgrep import pick_combos
+
+
+class TestMgrep(TestCase):
+    def setUp(self) -> None:
+        logging.basicConfig(level=logging.INFO, format="[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+
+    def _check_it(self, line, expected):
+        self.assertEqual(expected, list(pick_combos(line)))
+
+    def test_it(self):
+        self._check_it("своих старых", [])
+        self._check_it("просто набор", [])
+        self._check_it("гадкий я", [])  # игнорируем слова короче двух букв
+        self._check_it("неправильные схемы", ["Неправильные Схемы"])  # сохраняем согласованное число
+        self._check_it("по крайней мере", ["Крайняя Мера"])  # но при этом склоняем в именительный падеж
+        self._check_it("согласованных словосочетаний", ["Согласованные Словосочетания"])  # поддерживаем причастия
+        self._check_it("мудень проотвеченный", [])  # порядок строго прилагательное-существительное, а не наоборот
+        self._check_it("достаточно длинная строка для проверки итерации", ["Длинная Строка"])
