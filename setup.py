c54e395a2fa7867d3a00d94d97d1906eff3d2bd4
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2022-06-12 12:54:01 +0500

    Добавлено: установщик и утилита.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='epythets',
+    version='v0.0.3',
+    python_requires='>=3.8.0',
+    install_requires='pymorphy2',
+    packages=['epythets'],
+    url='https://github.com/strizhechenko/epythets',
+    license='MIT',
+    author='Oleg Strizhechenko',
+    author_email='oleg.strizhechenko@gmail.com',
+    description='С помощью pymorphy извлекаем из текстов множества эпитетов, нормализуем и сохраняем их в БД sqlite.',
+    entry_points={
+        "console_scripts": [
+            "epythets = epythets.__init__:main"
+        ],
+    },
+)
